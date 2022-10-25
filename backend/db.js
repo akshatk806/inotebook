7c99dcc8b80875ec442e444c66234e1cbb43211b
akshatk806 <akshatk806@gmail.com>
2022-10-25 22:23:02 +0530

    5. Express validator to validate the details

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@
 // Connecting to mongoDB
 
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/inotebook', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,37 @@
 const express=require('express')
 const router=express.Router();
 const User=require('../models/User');
+const { body, validationResult } = require('express-validator');
 
 // Create a user using: POST - "/api/auth" (this is the end point)
-router.post('/',(request,response)=>{
+router.post('/',[
+    body('email','Enter a valid email').isEmail(),
+    body('name','Enter a valid name').isLength({min:3}),
+    body('password','Password must contain 3 characters').isLength({min:3})
+],(request,response)=>{
+
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+        return response.status(400).json({ errors: errors.array() });
+    }
 
     // How to something in request body (Sending json in request body)
-    console.log(request.body); 
+    // console.log(request.body); 
     // response.send("Hello")
 
-    const user=User(request.body);
-    user.save();
-    response.send(request.body)
+    User.create({
+        email:request.body.email,
+        name:request.body.name,
+        password:request.body.password
+    }).then(user=>response.json(user))
+    .catch(err=>{
+        console.log(err);
+        response.json({
+            error:"Please enter a unique email",
+            message:err.message
+        });
+    });
 })
 
 module.exports=router
